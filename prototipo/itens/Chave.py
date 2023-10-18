9677a52be46979599667ec62e37fb72f82eb0fbb
Rodrigo Schwartz <rodrigo32003@gmail.com>
2023-10-17 22:01:02 -0300

    alterando caminho de assets

--- prototipo/itens/Chave.py
+++ prototipo/itens/Chave.py
@@ -3,7 +3,7 @@ import pygame
 class Chave(pygame.sprite.Sprite):
     def __init__(self, posicao):
         super().__init__()
-        self.image = pygame.image.load('.\projeto-final-grupo-1-23-2\im_chave.png')
+        self.image = pygame.image.load('.\Assets\im_chave.png')
         self.image.convert()
         self.image = pygame.transform.scale(self.image, (42, 42))
         self.rect = self.image.get_rect(center = posicao)
