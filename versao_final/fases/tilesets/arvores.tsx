a1d89eb80758770f5e1e041e95b0d3ca7427f5d6
Pedro Gimenez <pehqge@gmail.com>
2023-11-13 16:25:43 -0300

    começando arquivo final do projeto

---
+++ versao_final/fases/tilesets/arvores.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="arvores" tilewidth="64" tileheight="64" tilecount="5" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="51" height="53" source="../../../../../../Library/Mobile Documents/com~apple~CloudDocs/Downloads/Treasure Hunters 2/Palm Tree Island/Sprites/Back Palm Trees/Back Palm Tree Left 01.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="../../../../../../Library/Mobile Documents/com~apple~CloudDocs/Downloads/Treasure Hunters 2/Palm Tree Island/Sprites/Back Palm Trees/Back Palm Tree Regular 01.png"/>
+ </tile>
+ <tile id="2">
+  <image width="52" height="53" source="../../../../../../Library/Mobile Documents/com~apple~CloudDocs/Downloads/Treasure Hunters 2/Palm Tree Island/Sprites/Back Palm Trees/Back Palm Tree Right 01.png"/>
+ </tile>
+ <tile id="3">
+  <image width="39" height="32" source="../../../../../../Library/Mobile Documents/com~apple~CloudDocs/Downloads/Treasure Hunters 2/Palm Tree Island/Sprites/Front Palm Trees/Front Palm Tree Top 01.png"/>
+ </tile>
+ <tile id="5" x="0" y="0" width="32" height="32">
+  <image width="96" height="96" source="../../../../../../Library/Mobile Documents/com~apple~CloudDocs/Downloads/Treasure Hunters 2/Palm Tree Island/Sprites/Front Palm Trees/Front Palm Bottom and Grass (32x32).png"/>
+ </tile>
+</tileset>
