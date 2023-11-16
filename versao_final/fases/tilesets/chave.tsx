26f513761b22e9ff5a068c5a6d469640c2f685f6
Pedro Gimenez <pehqge@gmail.com>
2023-11-16 19:57:41 -0300

    atualizando tiles

--- versao_final/fases/tilesets/chave.tsx
+++ versao_final/fases/tilesets/chave.tsx
@@ -1,7 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="chave" tilewidth="24" tileheight="24" tilecount="1" columns="0">
- <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image width="24" height="24" source="../../../../../../Library/Mobile Documents/com~apple~CloudDocs/Downloads/Treasure Hunters 2/Merchant Ship/Sprites/Chest Key/Idle/1.png"/>
- </tile>
+<tileset version="1.10" tiledversion="1.10.2" name="chave" tilewidth="64" tileheight="64" tilecount="1" columns="1">
+ <image source="../../assets/source 2/Merchant Ship/Sprites/Chest Key/Idle/1_64x64_64x64.png" width="64" height="64"/>
 </tileset>
