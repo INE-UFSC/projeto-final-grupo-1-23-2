940a81d2b1813bdc1c421d5e59212e793a832f6b
Augusto Schweitzer <augustoschweitzer2@gmail.com>
2023-11-20 20:49:28 -0300

    Atualizado coracoes

--- versao_final/src/sistema/hud.py
+++ versao_final/src/sistema/hud.py
@@ -3,6 +3,8 @@ import pygame
 class HUD:
     def __init__(self, surface):
         #vida
+        self.__coracaoVazio = pygame.image.load('Assets/ui/coracao_vazio.png').convert_alpha()
+        self.__coracaoVazio = pygame.transform.scale(self.__coracaoVazio, (64,64))
         self.__coracao = pygame.image.load('Assets/ui/coracao.png').convert_alpha()
         self.__coracao = pygame.transform.scale(self.__coracao, (64,64))
         self.__vidas = []
@@ -23,8 +25,14 @@ class HUD:
 
     def mostrar_vida(self, vidas):
         for i in range(vidas):
-            x = i*70
+            x = i*50
             self.__vidas.append(self.__superficie.blit(self.__coracao, (x,10)))
+        if vidas != 5:
+            
+            #self.__vidas[4] = self.__superficie.blit(self.__coracaoVazio, (4*50,10))
+            for i in range(5 - vidas):
+                self.__vidas[i+vidas] = self.__superficie.blit(self.__coracaoVazio, ((vidas+i)*50,10))
+
         #self.__superficie.blit(self.__coracao, (20,10))
         pass
 
