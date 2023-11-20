2e38755a50d10a2afbde9b4f9a32208da820621e
Pedro Gimenez <pehqge@gmail.com>
2023-11-20 15:28:35 -0300

    Adicionado novos assets e nova fase em Tiled

--- versao_final/fases/tilesets/arvores.tsx
+++ versao_final/fases/tilesets/arvores.tsx
@@ -1,19 +1,13 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="arvores" tilewidth="64" tileheight="64" tilecount="5" columns="0">
+<tileset version="1.10" tiledversion="1.10.2" name="arvores" tilewidth="128" tileheight="136" tilecount="3" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image width="51" height="53" source="../../../../../../Library/Mobile Documents/com~apple~CloudDocs/Downloads/Treasure Hunters 2/Palm Tree Island/Sprites/Back Palm Trees/Back Palm Tree Left 01.png"/>
+  <image width="128" height="128" source="../../assets/source 1/terrain/palm_bg/bg_palm_1.png"/>
  </tile>
  <tile id="1">
-  <image width="64" height="64" source="../../../../../../Library/Mobile Documents/com~apple~CloudDocs/Downloads/Treasure Hunters 2/Palm Tree Island/Sprites/Back Palm Trees/Back Palm Tree Regular 01.png"/>
+  <image width="78" height="136" source="../../assets/source 1/terrain/palm_large/large_1.png"/>
  </tile>
  <tile id="2">
-  <image width="52" height="53" source="../../../../../../Library/Mobile Documents/com~apple~CloudDocs/Downloads/Treasure Hunters 2/Palm Tree Island/Sprites/Back Palm Trees/Back Palm Tree Right 01.png"/>
- </tile>
- <tile id="3">
-  <image width="39" height="32" source="../../../../../../Library/Mobile Documents/com~apple~CloudDocs/Downloads/Treasure Hunters 2/Palm Tree Island/Sprites/Front Palm Trees/Front Palm Tree Top 01.png"/>
- </tile>
- <tile id="5" x="0" y="0" width="32" height="32">
-  <image width="96" height="96" source="../../../../../../Library/Mobile Documents/com~apple~CloudDocs/Downloads/Treasure Hunters 2/Palm Tree Island/Sprites/Front Palm Trees/Front Palm Bottom and Grass (32x32).png"/>
+  <image width="80" height="103" source="../../assets/source 1/terrain/palm_small/small_1.png"/>
  </tile>
 </tileset>
