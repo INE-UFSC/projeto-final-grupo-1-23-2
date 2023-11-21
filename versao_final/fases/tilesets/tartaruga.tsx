151decd8bbcc1c5ed08595d6e8347cd555322921
Pedro Gimenez <pehqge@gmail.com>
2023-11-20 21:09:37 -0300

    algumas mudanças

--- versao_final/fases/tilesets/tartaruga.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="tartaruga" tilewidth="32" tileheight="32" tilecount="5" columns="5">
- <image source="../../../../game assets/tartaruga.png" width="160" height="32"/>
-</tileset>
--- versao_final/main.py
+++
@@ -1,9 +0,0 @@
-from src.sistema.sistema import Sistema
-
-
-def main():
-    Sistema().run()
-
-
-if __name__ == '__main__':
-    main()
