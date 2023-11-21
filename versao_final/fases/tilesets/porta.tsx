c93ac2b6dfccd2f62811268a8bffb24253d37b89
Pedro Gimenez <pehqge@gmail.com>
2023-11-21 00:59:29 -0300

    tiled inicial

--- versao_final/fases/tilesets/porta.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="porta" tilewidth="128" tileheight="128" tilecount="1" columns="0">
- <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image width="64" height="64" source="../../assets/source 2/Pirate Ship/Sprites/Decorations/Door/Opening/01_64x64_64x64.png"/>
- </tile>
-</tileset>
