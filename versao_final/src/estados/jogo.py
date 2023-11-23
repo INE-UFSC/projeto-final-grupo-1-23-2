65cd17611a6c20144dace033a9183ab4331d44d3
Tom Hunt <topmhunt0@gmail.com>
2023-11-23 15:49:00 -0300

    reveretndo jogo

--- versao_final/src/estados/jogo.py
+++ versao_final/src/estados/jogo.py
@@ -19,26 +19,20 @@ class Jogo(Estado):
         pass
     
     def update(self, event):
-            #condicional virou função abrir_menu()
-            self.abrir_menu(event)
-
-            self.__fase.update(event)
-            self.__colisao.update()
-            
-            #criada a função morto()
-            self.morto()
-            
-            self.__hud.mostrar_vida(self.__fase.vidas)
-            self.__hud.mostrar_chave(self.__fase.jogador_sprite.abrir_porta)
-
-    def abrir_menu(self, event):
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 self.game.define_estado('menu')
 
-    def morto(self):
+        self.__fase.update(event)
+        self.__colisao.update()
+        
         if self.__fase.vidas == 0:
             self.game.define_estado('gameover')
+        
+        self.__hud.mostrar_vida(self.__fase.vidas)
+        self.__hud.mostrar_chave(self.__fase.jogador_sprite.abrir_porta)
+
+
 
     def render(self):
         pass
