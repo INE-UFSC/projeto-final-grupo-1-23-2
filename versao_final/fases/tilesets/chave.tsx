f62fb6d26e3d96e443709a5812b743ee474eccb3
Pedro Gimenez <pehqge@gmail.com>
2023-11-24 14:51:30 -0300

    mudança da fase

--- versao_final/fases/tilesets/chave.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="chave" tilewidth="64" tileheight="64" tilecount="1" columns="0">
- <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image width="64" height="64" source="../../Assets/assets_forest/chave/chave1.png"/>
- </tile>
-</tileset>
