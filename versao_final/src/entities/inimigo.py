7eaac45bd14e39cd9b5ca70ceaf0477014cb3d1c
Rodrigo Schwartz <rodrigo32003@gmail.com>
2023-12-02 15:27:16 -0300

    retirando atributos e métodos da classe inimigo

--- versao_final/src/entities/inimigo.py
+++ versao_final/src/entities/inimigo.py
@@ -18,11 +18,6 @@ class Inimigo(pygame.sprite.Sprite):
         self.__velocidade = move_speed
         self.__direcao = pygame.math.Vector2((self.__velocidade), 0)
 
-        # vida inicial do inimigo
-        self.__vida_inicial = 3
-
-        self.__vivo = True
-        self.__x_y_morte = (self.__rect.x, self.__rect.y)
 
     def importar_assets(self):
         n = str(random.randint(1, 2))
@@ -45,17 +40,6 @@ class Inimigo(pygame.sprite.Sprite):
     def andar(self):
         self.__rect.x += self.__direcao.x * self.__velocidade
 
-    def dano_recebido(self):
-        self.__x_y_ataque = (self.__rect.x, self.__rect.y) #calcula posição que o inimigo está quando toma o hit (para ser usado quando o hit resulta em morte)
-        self.__vida_inicial -= 1
-        print(self.__vida_inicial)
-
-    def morte(self):
-        self.__rect.x = 10000
-        self.__rect.y = 10000
-        
-        print(self.__x_y_ataque[0])
-    
     def update(self):
         self.andar()
         self.animar()
@@ -69,7 +53,6 @@ class Inimigo(pygame.sprite.Sprite):
     def direcao(self):
         return self.__direcao
    
-    
     @property
     def rect(self):
         return self.__rect
@@ -83,35 +66,10 @@ class Inimigo(pygame.sprite.Sprite):
         self.__image = nova_imagem
 
     @property
-    def vida_inicial(self):
-        return self.__vida_inicial
-    
-    @vida_inicial.setter
-    def vida_inicial(self, vida_atual):
-        self.__vida_inicial = vida_atual
-
-    @property
-    def vivo(self):
-        return self.__vivo
-    
-    @vivo.setter
-    def vivo(self, vivo):
-        self.__vivo = vivo
-
-    @property
     def rect_final(self):
         return self.__rect
     
     @rect_final.setter
     def rect_final(self, rect):
         self.__rect_final = rect
-    
-    @property
-    def x_y_morte(self):
-        return self.__x_y_morte
-    
-    @x_y_morte.setter
-    def x_y_morte(self, x_y_morte):
-        self.__x_y_morte = x_y_morte
-    
     
