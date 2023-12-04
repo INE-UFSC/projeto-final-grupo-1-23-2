d0b00c9f54ba66e92c05e7dd277b7682100a5b0e
Tom Hunt <tomphunt0@gmail.com>
2023-12-04 02:00:01 -0300

    mapa de pântano praticamente finalizado

--- versao_final/assets/tiles/TILEMAP/tilesets/custom_tiles.tsx
+++ versao_final/assets/tiles/TILEMAP/tilesets/custom_tiles.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="custom_tiles" tilewidth="64" tileheight="64" tilecount="4" columns="0">
+<tileset version="1.10" tiledversion="1.10.2" name="custom_tiles" tilewidth="64" tileheight="64" tilecount="6" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="4">
   <image width="64" height="64" source="../../custom_tiles/half_tile_marrom.png"/>
@@ -12,5 +12,11 @@
  </tile>
  <tile id="7">
   <image width="64" height="64" source="../../custom_tiles/pocao_cinza.png"/>
+ </tile>
+ <tile id="8">
+  <image width="64" height="64" source="../../custom_tiles/pocao_vermelha.png"/>
+ </tile>
+ <tile id="9">
+  <image width="64" height="64" source="../../custom_tiles/pocao_azul.png"/>
  </tile>
 </tileset>
