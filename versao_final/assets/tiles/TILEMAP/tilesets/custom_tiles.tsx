3f12ab9e7b3ba74ebb53c9b3c9aedb49b0f908a7
AugustoSchw <augustoschweitzer2@gmail.com>
2023-12-04 18:14:42 -0300

    adicionada a fase 5 e algumas correções

--- versao_final/assets/tiles/TILEMAP/tilesets/custom_tiles.tsx
+++ versao_final/assets/tiles/TILEMAP/tilesets/custom_tiles.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="custom_tiles" tilewidth="64" tileheight="64" tilecount="6" columns="0">
+<tileset version="1.10" tiledversion="1.10.2" name="custom_tiles" tilewidth="64" tileheight="64" tilecount="23" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="4">
   <image width="64" height="64" source="../../custom_tiles/half_tile_marrom.png"/>
@@ -18,5 +18,56 @@
  </tile>
  <tile id="9">
   <image width="64" height="64" source="../../custom_tiles/pocao_azul.png"/>
+ </tile>
+ <tile id="12">
+  <image width="64" height="64" source="../../custom_tiles/doube-corner_dark_grass.png"/>
+ </tile>
+ <tile id="13">
+  <image width="64" height="64" source="../../custom_tiles/dark-grass.png"/>
+ </tile>
+ <tile id="14">
+  <image width="64" height="64" source="../../custom_tiles/agua_verde_escura.png"/>
+ </tile>
+ <tile id="28">
+  <image width="32" height="32" source="../../custom_tiles/door dark.png"/>
+ </tile>
+ <tile id="29">
+  <image width="64" height="64" source="../../custom_tiles/door_dark_BL.png"/>
+ </tile>
+ <tile id="30">
+  <image width="64" height="64" source="../../custom_tiles/door_dark_BR.png"/>
+ </tile>
+ <tile id="31">
+  <image width="64" height="64" source="../../custom_tiles/door_dark_TL.png"/>
+ </tile>
+ <tile id="32">
+  <image width="64" height="64" source="../../custom_tiles/door_dark_TR.png"/>
+ </tile>
+ <tile id="33">
+  <image width="64" height="64" source="../../custom_tiles/tall_dark_grass.png"/>
+ </tile>
+ <tile id="40">
+  <image width="64" height="64" source="../../custom_tiles/brown_step.png"/>
+ </tile>
+ <tile id="41">
+  <image width="64" height="64" source="../../custom_tiles/brown_step_UD.png"/>
+ </tile>
+ <tile id="42">
+  <image width="64" height="64" source="../../custom_tiles/dark-grass-UD.png"/>
+ </tile>
+ <tile id="45">
+  <image width="64" height="64" source="../../custom_tiles/half_tile_marrom_baixo.png"/>
+ </tile>
+ <tile id="49">
+  <image width="64" height="64" source="../../custom_tiles/bloco-agua-roxa.png"/>
+ </tile>
+ <tile id="51">
+  <image width="64" height="64" source="../../custom_tiles/half_tile_marrom_baixo_lava.png"/>
+ </tile>
+ <tile id="54">
+  <image width="64" height="64" source="../../custom_tiles/bloco-agua-verde-escuro.png"/>
+ </tile>
+ <tile id="55">
+  <image width="64" height="64" source="../../custom_tiles/hanging_lamp.png"/>
  </tile>
 </tileset>
