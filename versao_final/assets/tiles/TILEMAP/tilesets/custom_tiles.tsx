411b485f66ef8cf6d850576217a3ea83faeb3f00
Tom Hunt <tomphunt0@gmail.com>
2023-12-03 22:57:00 -0300

    adicionado 4o mapa (quase completo), e atualmente chamado de fase0

---
+++ versao_final/assets/tiles/TILEMAP/tilesets/custom_tiles.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="custom_tiles" tilewidth="64" tileheight="64" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="4">
+  <image width="64" height="64" source="../../custom_tiles/half_tile_marrom.png"/>
+ </tile>
+ <tile id="5">
+  <image width="64" height="64" source="../../custom_tiles/half_tile_marrom_esq.png"/>
+ </tile>
+ <tile id="6">
+  <image width="64" height="64" source="../../custom_tiles/half_tile_marrom_dir.png"/>
+ </tile>
+ <tile id="7">
+  <image width="64" height="64" source="../../custom_tiles/pocao_cinza.png"/>
+ </tile>
+</tileset>
--- versao_final/src/fase/fase.py
+++ versao_final/src/fase/fase.py
@@ -9,35 +9,36 @@ from src.sistema.hud import HUD
 import random
 import pytmx
 
+
 class Fase:
     def __init__(self, tiles, superficie, vida=5):
         self.__mapa = tiles
         self.__display_superficie = superficie
         self.__colisao = Colisao(self)
         self.__HUD = HUD(superficie, vida)
-        
+
         self.fase_setup(tiles)
-        
+
         self.__vidas = vida
         self.__passou_porta = False
-    
+
     def render(self):
         for tile in self.__background + self.__tiles:
             tile.draw(self.__display_superficie)
-            
+
         self.__HUD.render(self.__vidas, self.jogador_sprite.abrir_porta)
-        
+
     def update(self, event):
-      
+
         for objeto in self.__tiles:
             if hasattr(objeto, 'update'):
                 try:
                     objeto.update()
                 except TypeError:
-                    objeto.update(event) 
-                    
+                    objeto.update(event)
+
         self.__colisao.update()
-        
+
     def fase_setup(self, tmxdata):
         self.colide = pygame.sprite.Group()
         self.libera_chave = pygame.sprite.Group()
@@ -45,90 +46,93 @@ class Fase:
         self.espinhos = pygame.sprite.Group()
 
         self.chave = pygame.sprite.GroupSingle()
-        self.porta = pygame.sprite.GroupSingle() 
-        
-        self.jogador = pygame.sprite.GroupSingle() #so um jogador
+        self.porta = pygame.sprite.GroupSingle()
+
+        self.jogador = pygame.sprite.GroupSingle()  # so um jogador
         self.inimigo = pygame.sprite.Group()
-        
+
         self.inimigo_colisores = pygame.sprite.Group()
-        
+
         self.__tiles = []
         self.__background = []
         self.__bloco_chaves = []
-        
 
         for layer in tmxdata.visible_layers:
             if isinstance(layer, pytmx.TiledTileLayer):
                 for x, y, surf in layer.tiles():
                     x *= self.__mapa.tilewidth
                     y *= self.__mapa.tilewidth
-                    
+
                     if layer.name == 'libera_chave':
-                        self.__bloco_chaves.append(TileMap((x,y), surf, self.libera_chave))
-                    
+                        self.__bloco_chaves.append(
+                            TileMap((x, y), surf, self.libera_chave))
+
                     elif layer.name in ['terreno', 'ponte', 'struct_castelo', 'castelo', 'lava', 'nuvem_parkour']:
                         self.__tiles.append(TileMap((x, y), surf, self.colide))
-                        
-                    elif layer.name in ['arvores', 'dentro', 'decoracao', 'decoracao2', 'fundo_decorativo', 'borda_lava']:
-                        self.__tiles.append(TileMap((x, y), surf, self.ncolide))
-                        
+
+                    elif layer.name in ['arvores', 'dentro', 'decoracao', 'decoracao2', 'fundo_decorativo', 'borda_lava', 'pre-decoracao']:
+                        self.__tiles.append(
+                            TileMap((x, y), surf, self.ncolide))
+
                     elif layer.name == 'chave':
                         path = 'assets/tiles/chave'
 
-                        self.chave_sprite = Chave((x,y), path)
+                        self.chave_sprite = Chave((x, y), path)
                         self.chave.add(self.chave_sprite)
-                    
+
                     elif layer.name == 'porta':
-                        self.porta_sprite = Porta((x,y))
+                        self.porta_sprite = Porta((x, y))
                         self.porta.add(self.porta_sprite)
-                        
+
                     elif layer.name == 'player':
                         path = 'assets/entities/jogador/skin'
-                        self.jogador_sprite = Jogador((x, y),3, path)
+                        self.jogador_sprite = Jogador((x, y), 3, path)
                         self.jogador.add(self.jogador_sprite)
-                        
+
                     elif layer.name in ['morcego', 'mago', 'cavaleiro', 'caveira']:
-                        path_inimigo = f'assets/entities/inimigo/' + str(layer.name)
+                        path_inimigo = f'assets/entities/inimigo/' + \
+                            str(layer.name)
 
                         self.inimigo_sprite = Inimigo((x, y), 1, path_inimigo)
-                        self.inimigo.add(self.inimigo_sprite) 
-                        
+                        self.inimigo.add(self.inimigo_sprite)
+
                     elif layer.name == 'colisao_inimigo':
-                        TileMap((x,y), surf, self.inimigo_colisores)
+                        TileMap((x, y), surf, self.inimigo_colisores)
 
                     elif layer.name == 'dano':
-                        TileMap((x,y), surf, self.espinhos)
-                    
+                        TileMap((x, y), surf, self.espinhos)
+
             elif isinstance(layer, pytmx.TiledImageLayer):
                 if layer.name == 'nuvem':
-                    self.__background.append(Background(layer.image, animado=True))
+                    self.__background.append(
+                        Background(layer.image, animado=True))
                 else:
                     self.__background.append(Background(layer.image))
-                
-        self.__tiles += [self.porta, self.chave, self.libera_chave, self.jogador, self.inimigo, self.espinhos]
+
+        self.__tiles += [self.porta, self.chave, self.libera_chave,
+                         self.jogador, self.inimigo, self.espinhos]
 
     def reset(self):
         self.jogador_sprite.reset()
         self.chave_sprite.reset()
         [self.libera_chave.add(tile) for tile in self.__bloco_chaves]
-        
+
     @property
     def vidas(self):
         return self.__vidas
+
     @vidas.setter
     def vidas(self, numero):
         self.__vidas += numero
-    
+
     @property
     def tiles(self):
         return self.__tiles
-    
+
     @property
     def passou_porta(self):
         return self.__passou_porta
-    
+
     @passou_porta.setter
     def passou_porta(self, passou_porta):
         self.__passou_porta = passou_porta
-    
-    
